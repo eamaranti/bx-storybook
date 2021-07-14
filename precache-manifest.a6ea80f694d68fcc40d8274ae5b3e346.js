09065c7b41d4c59e3bff9960f98a0eb739977d71
Edmundo Amaranti <eamaranti@nectia.com>
2021-07-13 22:12:36 -0400

    sync

---
+++ precache-manifest.a6ea80f694d68fcc40d8274ae5b3e346.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8a82f30d1c9d46e9d9",
+    "url": "0.8720b66b.iframe.bundle.js"
+  },
+  {
+    "revision": "06ec13e9305dac318788",
+    "url": "4.18422891.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.18422891.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "caeb3676cc3e99f3d31a",
+    "url": "5.0c4a2957.iframe.bundle.js"
+  },
+  {
+    "revision": "5461649c19713a3c6fcf",
+    "url": "6.1ed0afa7.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.1ed0afa7.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fab58780021b3c909e4",
+    "url": "7.cef35919.iframe.bundle.js"
+  },
+  {
+    "revision": "bfd19de78707ea06d9836276932c6209",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "e6bd30d579882b03557c",
+    "url": "main.62ffda7d.iframe.bundle.js"
+  },
+  {
+    "revision": "8881f1f60eabe6552c93",
+    "url": "runtime~main.54724ff9.iframe.bundle.js"
+  },
+  {
+    "revision": "e6bd30d579882b03557c",
+    "url": "static/css/main.0ff590b1.chunk.css"
+  },
+  {
+    "revision": "365f797f893ddba60e64",
+    "url": "static/css/vendors~main.2a2177d1.chunk.css"
+  },
+  {
+    "revision": "7e9c6efd6f026b4e093b6d5ab6569b3c",
+    "url": "static/media/logo-blue.7e9c6efd.svg"
+  },
+  {
+    "revision": "365f797f893ddba60e64",
+    "url": "vendors~main.8add4d72.iframe.bundle.js"
+  },
+  {
+    "revision": "9b61be4d1fa42273d7df782cc0648934",
+    "url": "vendors~main.8add4d72.iframe.bundle.js.LICENSE.txt"
+  }
+]);
