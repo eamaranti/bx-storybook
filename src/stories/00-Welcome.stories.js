88028aaf1312245cafdd4e5aeb3c8111561894b1
Edmundo Amaranti <eamaranti@nectia.com>
2021-08-11 11:56:53 -0400

    datadotr

--- src/stories/00-Welcome.stories.js
+++ src/stories/00-Welcome.stories.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import Welcome from './Welcome'
+import { datadogRum } from '@datadog/browser-rum'; 
+
+datadogRum.init({
+  applicationId: 'afaf967b-9afa-401b-ae86-57d46d981e4f',
+  clientToken: 'pubcd4c15328db9846d075ff357f4d0a828',
+  site: 'datadoghq.com',
+   service: 'prueba-1',
+  //  env: 'production',
+  //  version: '1.0.0',
+  sampleRate: 100,
+  trackInteractions: true,
+})
 
 export default {
   title: 'Welcome',
--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3384a06b9167d2199772532b27a815e4.js"
+  "precache-manifest.00530448d1991a6399a59d814c86f82e.js"
 );
 
 self.addEventListener('message', (event) => {
