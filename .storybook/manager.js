4e71b0f452af38c2d38be76ee85477507471f417
Edmundo Amaranti <eamaranti@nectia.com>
2021-08-11 11:57:46 -0400

    datadotr

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -8,3 +8,15 @@ import bxthemeDark from './bxtheme-dark';
 addons.setConfig({
   theme: bxthemeDark,
 });
+import { datadogRum } from '@datadog/browser-rum'; 
+
+datadogRum.init({
+  applicationId: 'afaf967b-9afa-401b-ae86-57d46d981e4f',
+  clientToken: 'pubcd4c15328db9846d075ff357f4d0a828',
+  site: 'datadoghq.com',
+   service: 'prueba-1',
+  //  env: 'production',
+  //  version: '1.0.0',
+  sampleRate: 100,
+  trackInteractions: true,
+})
